2da6bce2a554723b84237d40d614a17c1dff9c0e
char-1ee <xingjianli59@gmail.com>
2022-05-09 00:07:27 +0800

    Update Q17

--- Q17.java
+++ Q17.java
@@ -2,6 +2,7 @@
  * Print from 1 to the max n-digit number.
  */
 public class Q17 {
+    // ================= method 1 ===================
     public static void print1ToMaxNDigits(int n) {
         if (n <= 0) {
             System.out.println("Invalid input");
@@ -9,7 +10,7 @@ public class Q17 {
         }
 
         char[] num = new char[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) { // default value in array is
             num[i] = '0';
         }
 
@@ -26,7 +27,7 @@ public class Q17 {
         for (int i = num.length - 1; i >= 0; i--) {
             int sum = (num[i] - '0') + carry;
             if (i == num.length - 1) {
-                sum++; 
+                sum++;
             }
             if (sum >= 10) {
                 if (i == 0) {
@@ -59,23 +60,47 @@ public class Q17 {
         System.out.print(" ");
     }
 
+    // ===================== method 2 =========================
+    public static void print1ToMaxNDigits_2(int n) {
+        if (n <= 0) {
+            System.out.println("Invalid input");
+            return;
+        }
+
+        char[] num = new char[n];
+        helper(num, n, 0);
+        System.out.println();
+
+    }
+
+    private static void helper(char[] num, int length, int idx) {
+        // recursion terminates
+        if (idx == length) {
+            printNumber(num);
+            return;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            num[idx] = (char) (i + '0');
+            helper(num, length, idx + 1);
+        }
+    }
+
     /** Unit testing */
     private static void test(int n) {
-        System.out.println("Test for n=" + n + " begins:");
-
+        System.out.println("======== n=" + n + " =========");
         print1ToMaxNDigits(n);
-
-        System.out.println("Test for n=" + n + " ends:");
+        print1ToMaxNDigits_2(n);
     }
 
     /** Testing driver */
     public static void main(String[] args) {
         test(1);
         test(2);
-        test(3);   
+        test(3);
         test(0);
-        test(-1);  
-        
-        test(10);
+        test(-1);
+
+        // test(10);
     }
 }
