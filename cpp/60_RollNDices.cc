19d5771ed0e991682e315a02c482be2646ee9a59
char-1ee <xingjianli59@gmail.com>
2022-08-18 21:30:47 +0800

    Add solution

--- cpp/60_RollNDices.cc
+++ cpp/60_RollNDices.cc
@@ -1,21 +1,22 @@
 // Q60: Indexes of n dices.
 // Description: Roll n dices and sum of their indexes is s. Input n, print out all probabilities of
 // value of s.
+// Examples:
+// Input: 1, output: [0.16667,0.16667,0.16667,0.16667,0.16667,0.16667]
+// input: 2, output:
+// [0.02778,0.05556,0.08333,0.11111,0.13889,0.16667,0.13889,0.11111,0.08333,0.05556,0.02778]
 
 #include <bits/stdc++.h>
 using namespace std;
 
+const int nSides = 6;
 
+// ===== DP based on iteration =====
+void Roll(int n) {}
 
 void Test(int n) {
     printf("Test for %d begins:\n", n);
-
-    printf("Test for solution1\n");
-    PrintProbability1(n);
-
-    printf("Test for solution2\n");
-    PrintProbability2(n);
-
+    Roll(n);
     printf("\n");
 }
 
